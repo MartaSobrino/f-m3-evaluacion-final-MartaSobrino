849fda36d85dbc9f5fb8530eb28101177e79be15
Marta Sobrino <marta.sobrino.web@gmail.com>
2019-05-27 10:30:40 +0200

    select responsive

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -13,19 +13,15 @@ class Filter extends React.Component{
                         <i className="glyphicon glyphicon-search fas fa-search"></i>
                         <input type="text" name="filter__name" id="filter" className="seacrh__input" onChange={changeInput} placeholder="intrduce aquí el nombre" />
                     </div>
-                </div>
-                <div className="search__select">
-                    <label className="label__input" htmlFor="house__select">Elige la casa que más te guste</label>
-                    <select name="" id="house__select" onChange={changeSelect} >
+                     <label className="label__select" htmlFor="house__select">Elige la casa que más te guste</label>
+                    <select name="" id="filter__select" onChange={changeSelect} >
                         <option value="all">Todo</option>
                         <option value="Gryffindor">Gyiffindor</option>
                         <option value="Slytherin">Slytherin</option>
                         <option value="Hufflepuff">Hufflepuff</option>
                         <option value="Ravenclaw">Ravenclaw</option>
-                    </select>
-                </div>
-                   
-                
+                    </select>   
+                </div>           
             </div>
         );
     }
