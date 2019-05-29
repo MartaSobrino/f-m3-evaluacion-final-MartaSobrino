74e453e545c698180b3659afbda0943e73bb4e1c
Marta Sobrino <marta.sobrino.web@gmail.com>
2019-05-29 09:10:37 +0200

    added new docs

---
+++ docs/precache-manifest.b963a21e7a0ccaa0b5da2a6b4fd847bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fccba216633830d5ab633ef161a10c3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6b1804d248fe6c6746a6",
+    "url": "./static/css/main.486322d4.chunk.css"
+  },
+  {
+    "revision": "4a7f65c326f836a13046",
+    "url": "./static/js/2.5d55156b.chunk.js"
+  },
+  {
+    "revision": "6b1804d248fe6c6746a6",
+    "url": "./static/js/main.e9c38b4a.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "e7a7220bb24f7f13cffbf81223631cf3",
+    "url": "./static/media/logo.e7a7220b.png"
+  }
+]);
